47da46e3918d0563966b5b02face0424b639c464
徐无敌 <1150095145@qq.com>
2021-05-10 17:20:28 +0800

    代码框架修改

---
+++ app/src/main/java/com/example/brainking/action/KeyboardAction.java
@@ -0,0 +1,55 @@
+package com.example.brainking.action;
+
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+/**
+ * @author : 徐无敌
+ * date   : 2021/5/1015:43
+ * desc   :软键盘相关
+ */
+public interface KeyboardAction {
+
+    /**
+     * 显示软键盘
+     */
+    default void showKeyboard(View view) {
+        if (view == null) {
+            return;
+        }
+        InputMethodManager manager = (InputMethodManager) view.getContext()
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (manager != null) {
+            manager.showSoftInput(view, 0);
+        }
+    }
+
+    /**
+     * 隐藏软键盘
+     */
+    default void hideKeyboard(View view) {
+        if (view == null) {
+            return;
+        }
+        InputMethodManager manager = (InputMethodManager) view.getContext()
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (manager != null) {
+            manager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
+
+    /**
+     * 切换软键盘
+     */
+    default void toggleSoftInput(View view) {
+        if (view == null) {
+            return;
+        }
+        InputMethodManager manager = (InputMethodManager) view.getContext()
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (manager != null) {
+            manager.toggleSoftInput(0, 0);
+        }
+    }
+}
