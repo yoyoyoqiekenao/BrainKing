21560a7e0c6b5094adbe72f6eda96d1ec38b8374
xuwudi_home <1150095145@qq.com>
2021-05-14 00:38:27 +0800

    首页界面绘制

--- app/src/main/java/com/example/brainking/home/home/HomeFragment.java
+++ app/src/main/java/com/example/brainking/home/home/HomeFragment.java
@@ -1,20 +1,33 @@
 package com.example.brainking.home.home;
 
 
-
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+
 import com.example.brainking.R;
 import com.example.brainking.base.BaseFragment;
 import com.gyf.immersionbar.ImmersionBar;
 
 
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class HomeFragment extends BaseFragment   {
+public class HomeFragment extends BaseFragment {
     @BindView(R.id.view)
     View mView;
+    @BindView(R.id.rl_language)
+    RelativeLayout rl_language;
+    @BindView(R.id.rootView)
+    LinearLayout rootView;
+
+    private PopupWindow mPop;
 
 
     @Override
@@ -24,8 +37,10 @@ public class HomeFragment extends BaseFragment   {
 
     @Override
     protected void initView() {
-        ButterKnife.bind(this,getView());
+        ButterKnife.bind(this, getView());
         ImmersionBar.with(getActivity()).statusBarView(mView).init();
+
+        rl_language.setOnClickListener(this);
     }
 
     @Override
@@ -33,4 +48,26 @@ public class HomeFragment extends BaseFragment   {
 
     }
 
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.rl_language) {
+            showPop();
+        }
+    }
+
+    private void showPop() {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.pop_grade, null);
+        mPop = new PopupWindow(getContext());
+        mPop.setContentView(view);
+        mPop.setWidth(ViewGroup.LayoutParams.WRAP_CONTENT);
+        mPop.setHeight(ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        // 设置PopupWindow的背景
+        mPop.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        mPop.setOutsideTouchable(true);
+        mPop.setTouchable(true);
+        mPop.setFocusable(true); // pop 显示时， 不让外部 view 响应点击事件
+
+        mPop.showAtLocation(rootView, Gravity.CENTER, 0, 0);
+    }
 }
