d357d6648f4e995a4483543b59df07608cacded5
xuwudi_home <1150095145@qq.com>
2021-11-08 22:25:04 +0800

    bugly接入

--- app/src/main/java/com/example/brainking/MyApplication.java
+++ app/src/main/java/com/example/brainking/MyApplication.java
@@ -3,6 +3,9 @@ package com.example.brainking;
 import android.app.Application;
 import android.content.Context;
 
+import com.tencent.bugly.Bugly;
+import com.tencent.bugly.crashreport.CrashReport;
+
 /**
  * @author : 徐无敌
  * date   : 2021/6/1514:04
@@ -15,5 +18,7 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         app = getApplicationContext();
+        Bugly.init(getApplicationContext(), "a4b778ca17", false);
+
     }
 }
