aa044b2556ae71f7e6f4abe808d842940a6205e0
xuwudi_home <1150095145@qq.com>
2021-11-30 22:48:36 +0800

    关于我们增加隐私协议和用户协议

--- app/src/main/java/com/example/brainking/mine/about/AboutActivity.java
+++ app/src/main/java/com/example/brainking/mine/about/AboutActivity.java
@@ -1,9 +1,11 @@
 package com.example.brainking.mine.about;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
 
+import com.example.brainking.MyWebActivity;
 import com.example.brainking.R;
 import com.example.brainking.base.BaseActivity;
 import com.example.brainking.base.BasePresenter;
@@ -18,6 +20,10 @@ public class AboutActivity extends BrainActivity implements View.OnClickListener
     View mView;
     @BindView(R.id.rl_back)
     RelativeLayout rlBack;
+    @BindView(R.id.rl_1)
+    RelativeLayout rl_1;
+    @BindView(R.id.rl_2)
+    RelativeLayout rl_2;
 
     @Override
     protected BasePresenter createPresenter() {
@@ -37,6 +43,8 @@ public class AboutActivity extends BrainActivity implements View.OnClickListener
         ImmersionBar.with(this).statusBarView(mView).init();
 
         rlBack.setOnClickListener(this);
+        rl_1.setOnClickListener(this);
+        rl_2.setOnClickListener(this);
     }
 
 
@@ -44,6 +52,16 @@ public class AboutActivity extends BrainActivity implements View.OnClickListener
     public void onClick(View view) {
         if (view.getId() == R.id.rl_back) {
             finish();
+        } else if (view.getId() == R.id.rl_1) {
+            Intent intent = new Intent(this, MyWebActivity.class);
+            intent.putExtra("url", "http://www.wdsd66.cn/protocol/privacy_agreement.html");
+            intent.putExtra("title", "隐私协议");
+            startActivity(intent);
+        } else if (view.getId() == R.id.rl_2) {
+            Intent intent = new Intent(this, MyWebActivity.class);
+            intent.putExtra("url", "http://www.wdsd66.cn/protocol/user_agreement.html");
+            intent.putExtra("title", "用户协议");
+            startActivity(intent);
         }
     }
 }
